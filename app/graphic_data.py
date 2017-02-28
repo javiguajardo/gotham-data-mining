46bd3bd9a0b10b257152601385b30d4070917017
guajardo <a282057@uach.mx>
2017-02-27 22:05:37 -0700

    Se hace un refactor de metodo que liga a un metodo con una ofensa

--- app/graphic_data.py
+++ app/graphic_data.py
@@ -6,27 +6,30 @@ def open_file(fileName):
     data = pd.read_csv(fileName)
     return data
 
-def gun_equals_homicide(data):
+def method_equals_offense(data, method_index, weapon, crime_type):
     offense = data['offense'].value_counts()
     method = data['method'].value_counts()
     offense_keys = offense.keys()
     method_keys = method.keys()
-    guns_count = []
-
+    method_count = []
 
     for k in offense_keys:
         subset = data.loc[data['offense'] == k]
-        count = subset.loc[subset['method'] == method_keys[1]]['method'].value_counts()
-        guns_count.append(count)
+        count = subset.loc[subset['method'] == method_keys[method_index]]['method'].value_counts()
+        method_count.append(count)
+
+    print(method_keys)
+    print(method_count)
 
     sns.set_style("whitegrid")
-    plt.ylabel('OFFENSES WITH GUNS')
+    plt.ylabel("OFFENSES USING %s" % (weapon))
     plt.xlabel('OFFENSES')
-    plt.title('GUNS EQUALS HOMICIDE')
-    ax = sns.barplot(x=offense_keys, y=guns_count)
+    plt.title("%s EQUALS %s" % (weapon, crime_type))
+    ax = sns.barplot(x=offense_keys, y=method_count)
     plt.show()
 
 if __name__ == '__main__':
     data = open_file("../resources/crime_with_cleaning.csv")
-    gun_equals_homicide(data)
-    print(data)
+    #method_equals_offense(data, 1, 'GUNS', 'HOMICIDE')
+    #method_equals_offense(data, 2, 'KNIFES', 'HOMICIDE')
+    method_equals_offense(data, 0, 'OTHERS', 'SEX ABUSE')
